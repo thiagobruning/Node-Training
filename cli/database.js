610f8b5af609cfa20c1df1b070a9ad53a70cd114
Thiago Bruning <thi.bruning@hotmail.com>
2020-05-12 10:54:36 -0300

    Working with cli commands

--- cli/database.js
+++ cli/database.js
@@ -1,61 +1,80 @@
-const { readFile, writeFile } = require('fs')
-const { promisify } = require('util');
+/* 
+*/
 
-const readFileAsync = promisify(readFile)
-const writeFileAsync = promisify(writeFile)
+const { writeFile, readFile } = require('fs');
+const { promisify } = require('util');
+const [writeFileAsync, readFileAsync] = [
+  promisify(writeFile),
+  promisify(readFile),
+];
 
 class Database {
+  constructor() {
+    this.FILENAME = 'heroes.json';
+  }
 
-    constructor () {
-        this.NOME_ARQUIVO = "heroes.json"
+  async obterArquivo() {
+    const arquivo = await readFileAsync(this.FILENAME);
+    return JSON.parse(arquivo.toString());
+  }
+
+  async escreverArquivo(dados) {
+    await writeFileAsync(this.FILENAME, JSON.stringify(dados));
+    return true;
+  }
+
+  async cadastrar(heroi) {
+    const dados = await this.obterArquivo();
+    //workaround para simular um id
+    const id = heroi.id <= 2 ? heroi.id : Date.now();
+    const heroiComId = {
+      ...heroi,
+      id,
+    };
+
+    return await this.escreverArquivo([...dados, heroiComId]);
+  }
+
+  async listar(id) {
+    const dados = await this.obterArquivo();
+    // se nao passar o id, traz tudo
+    return dados.filter(item => (id ? item.id == id : true));
+  }
+
+  async atualizar(id, atualizacoes) {
+    const dados = await this.obterArquivo();
+    const indice = dados.findIndex(item => item.id === parseInt(id));
+    if (indice === -1) {
+      throw Error('heroi não existe!');
     }
 
-    async ObterDadosArquivo()
-    {
-        const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
-        return JSON.parse(arquivo.toString())
+    const atual = dados[indice];
+    dados.splice(indice, 1);
+
+    //workaround para remover valores undefined do objeto
+    const objAtualizado = JSON.parse(JSON.stringify(atualizacoes));
+    const dadoAtualizado = Object.assign({}, atual, objAtualizado);
+
+    return await this.escreverArquivo([...dados, dadoAtualizado]);
+  }
+
+  async remover(id) {
+    if (!id) {
+      await this.escreverArquivo([]);
+      return true;
     }
 
-    async escreverArquivo(dados)
-    {
-        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
-        return true
+    const dados = await this.obterArquivo();
+
+    const indice = dados.findIndex(item => item.id === parseInt(id));
+    if (indice === -1) {
+      throw Error('heroi não existe!');
     }
-
-    async cadastrar(heroi)
-    {
-        const dados = await this.ObterDadosArquivo()
-        const id = heroi.id <= 2 ? heroi.id : Date.now();
-
-        // concatenando o id com o objeto de heroi vindo de fora
-        const heroiComId = {
-            id, 
-            ...heroi
-        }
-
-        /*
-        * concatenando o conteudo que ja tinha no arquivo, com o novo heroi,
-        * assim gerando um novo array
-        */
-        const novoArray = [
-            ...dados, 
-            heroiComId
-        ]
-
-        const resultado = await this.escreverArquivo(novoArray)
-        return resultado
-    }
-
-    async listar(id)
-    {
-        const dados = await this.ObterDadosArquivo()
-
-        // se passou iD, traz quem tiver o id. Se não passou return true e traz todos
-        const dadosFiltrados =  dados.filter(item => (id ? (item.id === id) : true))
-         
-        return dadosFiltrados
-    }
-
+    const atual = dados[indice];
+    dados.splice(indice, 1);
+    await this.escreverArquivo(dados);
+    return true;
+  }
 }
 
-module.exports = new Database()
+module.exports = new Database();
--- cli/test.js
+++ cli/test.js
@@ -1,25 +1,45 @@
-const { deepEqual, ok } = require('assert')
-const database = require('./database')
+const { deepEqual, ok } = require('assert');
+const Database = require('./database');
+const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'speed', id: 1 };
+const DEFAULT_ITEM_ATUALIZAR = {
+  nome: 'Lanterna Verde',
+  poder: 'Anel do poder',
+  id: 2,
+};
 
-const DEFAULT_HERO = {
-    id: 1,
-    name: 'Flash',
-    power: 'Speed'
-}
+describe('Suite de manipulação de herois', () => {
+  before(async () => {
+    await Database.remover();
+    await Database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    await Database.cadastrar(DEFAULT_ITEM_ATUALIZAR);
+  });
 
-describe('Suite de manipulação de Herois', () => {
-    it('Pesquisar herois utilizando arquivos', async () =>{
-        const expected = DEFAULT_HERO
-        
-        // entre colchetes trás apenas a primeira posição
-        const [resultado] = await database.listar(expected.id) 
-        deepEqual(resultado, expected)
-    })
-    
-    it('Deve cadastrar um heroi usando arquivos', async () => {
-        const expected = DEFAULT_HERO
-        const resultado = await database.cadastrar(DEFAULT_HERO)
-        const actual = await database.listar(DEFAULT_HERO.id)
-        deepEqual(actual, expected)
-    })
-})
+  it('deve cadastrar um heroi', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    await Database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+
+    const [realResult] = await Database.listar(expected.id);
+    deepEqual(realResult, expected);
+  });
+
+  it('deve listar um heroi pelo id', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const result = await Database.listar(1);
+    deepEqual(result[0], expected);
+  });
+
+  it('deve atualizar um heroi pelo id', async () => {
+    const expected = {
+      ...DEFAULT_ITEM_ATUALIZAR,
+      nome: 'Batman',
+      poder: 'ricão',
+    };
+    await Database.atualizar(expected.id, {
+      nome: expected.nome,
+      poder: expected.poder,
+    });
+
+    const [realResult] = await Database.listar(expected.id);
+    deepEqual(realResult, expected);
+  });
+});
